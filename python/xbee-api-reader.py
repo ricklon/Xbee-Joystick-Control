7ee141880ff68b78ce282690d721aeb69adc8b72
Ricklon <rick.rickanderson@gmail.com>
2011-11-07 16:42:44 -0500

    Xbee Breakout Board added.

--- python/xbee-api-reader.py
+++ python/xbee-api-reader.py
@@ -6,8 +6,9 @@ from xbee import XBee
 import serial
 
 
-ser = serial.Serial('/dev/tty.usbserial-FTT3JNCK', 9600)
 #ser = serial.Serial('/dev/ttyUSB0', 9600)
+#ser = serial.Serial('/dev/tty.usbserial-FTT3JNCK', 9600)
+ser = serial.Serial('/dev/tty.SLAB_USBtoUART', 9600)
 
 xbee = XBee(ser)
 
