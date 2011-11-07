8fbaae889b7a05d290295809729bd1b19c22fc63
Ricklon <rick.rickanderson@gmail.com>
2011-11-07 13:42:38 -0500

    Added xbee-api-reader.py this is the example from: http://code.google.com/p/python-xbee/

---
+++ python/xbee-api-reader.py
@@ -0,0 +1,22 @@
+"""
+Continuously read the serial port and process IO data received from a remote XBee.
+"""
+
+from xbee import XBee
+import serial
+
+
+ser = serial.Serial('/dev/tty.usbserial-FTT3JNCK', 9600)
+#ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+xbee = XBee(ser)
+
+# Continuously read and print packets
+while True:
+    try:
+        response = xbee.wait_read_frame()
+        print response
+    except KeyboardInterrupt:
+        break
+        
+ser.close()
